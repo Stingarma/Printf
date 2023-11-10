30337229ad810c6890e91f525c30fb2f43792543
Stingarma <144959747+Stingarma@users.noreply.github.com>
2023-11-10 09:45:10 +0100

    printf maj V0.1.2

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,68 @@
 /*   By: lsaumon <lsaumon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 12:33:37 by lsaumon           #+#    #+#             */
-/*   Updated: 2023/11/09 12:33:38 by lsaumon          ###   ########.fr       */
+/*   Updated: 2023/11/10 09:44:18 by lsaumon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "printf.h"
+
+void	ft_printchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_printstr(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i])
+	{
+		write (1, s[i], 1);
+		i++;
+	}
+}
+void	ft_printptr(void *p)
+{
+	
+}
+
+int ft_printf(const char *str, ...)
+{
+	va_list	args;
+	char	c;
+	char	*s;
+	void	*p;
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == "%")
+		{
+			if (str[i + 1] == 'c')
+			{
+				c = va_arg(args, char);
+				ft_printchar(c);
+			}
+			if (str[i + 1] == 's')
+			{
+				s = va_arg(args, char *);
+				ft_printstr(s);
+			}
+			if (str[i + 1] == 'p')
+			{
+				p = va_arg(args, void *);
+				
+			}
+			if (str[i + 1] == 'd' || str[i + 1] == 'i')
+			{
+				va_arg(args, int)
+				
+			}
+		}
+	}
+}
--- libftprintf.h
+++ libftprintf.h
@@ -6,11 +6,16 @@
 /*   By: lsaumon <lsaumon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 12:34:03 by lsaumon           #+#    #+#             */
-/*   Updated: 2023/11/09 12:35:07 by lsaumon          ###   ########.fr       */
+/*   Updated: 2023/11/10 09:17:52 by lsaumon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINT_H
 # define PRINTF_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+
+int ft_printf(const char *str, ...);
 
 #endif
