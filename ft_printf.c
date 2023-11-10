94d9233e35c72459d257703fa376b829dd47d5c7
Stingarma <144959747+Stingarma@users.noreply.github.com>
2023-11-10 11:27:35 +0100

    maj printf  V0.1.3

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: lsaumon <lsaumon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 12:33:37 by lsaumon           #+#    #+#             */
-/*   Updated: 2023/11/10 09:44:18 by lsaumon          ###   ########.fr       */
+/*   Updated: 2023/11/10 11:26:56 by lsaumon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,43 +30,96 @@ void	ft_printstr(char *s)
 		i++;
 	}
 }
-void	ft_printptr(void *p)
+
+char	*ft_setbase(char type)
 {
-	
+	if (type == 'u' || type == 'd' || type == 'i')
+		return ("0123456789");
+	if (type == 'x' || type == 'p')
+		return ("0123456789abcdef");
+	if (type == 'X')
+		return ("0123456789ABCDEF");
 }
 
-int ft_printf(const char *str, ...)
+int		ft_printnbr(long nb, const char type)
+{
+	char	*base;
+	int		base_size;
+	
+	base = ft_strlen(nb);
+	base_size = 0;
+	if (nb == -2147483648)
+	{
+		ft_printchar('-');
+		ft_printchar('2');
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		ft_printchar('-');
+		nb *= -1;
+	}
+	if (nb >= x)
+	{
+		ft_printnbr(nb / x);
+		ft_printnbr(nb % x);
+	}
+	else
+	{
+		ft_printchar(nb + 48);
+	}
+}
+
+int		ft_printf(const char *str, ...)
 {
 	va_list	args;
 	char	c;
 	char	*s;
 	void	*p;
 	int		i;
+	int		d;
+	unsigned int	u;
+	int		index;
 
 	i = 0;
-	while (str[i])
+	while (str[index])
 	{
-		if (str[i] == "%")
+		if (str[index] == "%")
 		{
-			if (str[i + 1] == 'c')
+			if (str[index + 1] == 'c')
 			{
 				c = va_arg(args, char);
 				ft_printchar(c);
 			}
-			if (str[i + 1] == 's')
+			if (str[index + 1] == 's')
 			{
 				s = va_arg(args, char *);
 				ft_printstr(s);
 			}
-			if (str[i + 1] == 'p')
+			if (str[index + 1] == 'p')
 			{
 				p = va_arg(args, void *);
+				ft_printnbr(p);
+			}
+			if (str[index + 1] == 'd' || str[index + 1] == 'i')
+			{
+				i = va_arg(args, int);
+				d = va_arg(args, int);
+				ft_printnbr(i);
+				ft_printnbr(d);
+			}
+			if (str[i + 1] == 'u')
+			{
+				u = va_arg(args, unsigned int);
 				
 			}
-			if (str[i + 1] == 'd' || str[i + 1] == 'i')
+			if (str[index + 1] == 'x' || str[index + 1] == 'X')
 			{
-				va_arg(args, int)
 				
+			}
+			if (str[index + 1] == "%")
+			{
+				write(1, "%", 1);
 			}
 		}
 	}
